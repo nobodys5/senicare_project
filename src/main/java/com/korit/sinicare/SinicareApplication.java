da54cc00c01deaa4c3734d42683f2d92e7061b60
nobodys5 <samudi2@naver.com>
2024-09-10 10:32:19 +0900

    first commit

---
+++ src/main/java/com/korit/sinicare/SinicareApplication.java
@@ -0,0 +1,13 @@
+package com.korit.sinicare;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SinicareApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SinicareApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/korit/sinicare/SinicareApplicationTests.java
@@ -0,0 +1,13 @@
+package com.korit.sinicare;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SinicareApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
